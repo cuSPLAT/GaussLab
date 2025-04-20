d2020d527d425ccf880f3178e3cead4c399017fa
Abdelrhman Zain <2101646@eng.asu.edu.eg>
2025-04-20 15:07:40 +0200

    Update dicom_reader.h

--- src/data_reader/dicom_reader.h
+++ src/data_reader/dicom_reader.h
@@ -1,37 +1,40 @@
 #include <atomic>
+#include <string>
+#include <vega/dicom/file.h>
+
 #define DEFAULT_DICOM_DICTIONARY "/home/Abdelrahman/projects/gradproject/GausStudio/dictionary.txt"
 
 #define getDataManipulator(file, tag) file.data_set()->element<tag>()->manipulator()
 
-#include <string>
-#include <thread>
 
-#include <vega/dicom/file.h>
 
+
+struct Geometry {
+    int   nVoxelX, nVoxelY, nVoxelZ;
+    float sVoxelX, sVoxelY, sVoxelZ;
+    float dVoxelX, dVoxelY, dVoxelZ;
+    float *offOrigX, *offOrigY, *offOrigZ;
+    float* DSO;
+    int   nDetecU, nDetecV;
+    float sDetecU, sDetecV;
+    float dDetecU, dDetecV;
+    float *offDetecU, *offDetecV;
+    float* DSD;
+    float* dRoll;
+    float* dPitch;
+    float* dYaw;
+    float unitX, unitY, unitZ;
+    float alpha, theta, psi;
+    float* COR;
+    float maxLength;
+    float accuracy;
+};
 
 class DicomReader {
     std::vector<vega::dicom::File> dicomFiles;
 
-    std::vector<std::thread> threads;
-
 public:
-    std::atomic<std::uint64_t> loadingProgress {0};
-    // initialized with a large number so the loading bar can work instantly
-    std::atomic<std::uint64_t> totalSize {10000000};
-
-    struct DicomData {
-        float* buffer;
-        int width, length, height;
-
-        std::atomic_flag readable {};
-    };
-
-    DicomData loadedData {};
-
     DicomReader(const std::string& dictionary = "");
     
-    void readDirectory(const std::string& path);
-
-    void launchReaderThread(const std::string& path);
-    void cleanupThreads();
+    Geometry readDirectory(const std::string& path);
 };
